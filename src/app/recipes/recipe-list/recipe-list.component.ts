21ad878ff585c3267ab64144feeaf83024cba489
Loes <s1101632@student.hsleiden.nl>
2019-03-02 14:11:27 +0100

    Finished section 3

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test', 'Test', 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_960/https://storage.googleapis.com/gen-atmedia/3/2018/08/9bbf07cb31d5ed6f536454f6792d96f16c7bc26b.jpeg'),
+    new Recipe('A Test', 'Test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
